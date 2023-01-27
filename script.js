3b99c76fdebeb25323ab4bfc378d6b8a39c4a317
maouanaa <sofienmw@gmail.com>
2023-01-27 11:28:06 +0100

    first comit

---
+++ script.js
@@ -0,0 +1,15 @@
+// alert message function
+function aler() {
+    alert("Your cart is empty");
+}
+// function change picture
+function over(e) {
+    e.src = "./image/succulents-2.jpg"
+}
+function out(e) {
+    e.src = "./image/succulents-1.jpg"
+}
+// function remove mesagge div
+function remov(id) {
+    document.getElementById(id).remove()
+}
